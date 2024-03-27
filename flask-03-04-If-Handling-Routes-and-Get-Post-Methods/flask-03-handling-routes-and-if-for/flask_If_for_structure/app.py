fcab5bfee35870d4ef298b43ee0aa7524e864313
serkan210405 <serkanyagli2023@gmail.com>
2024-03-27 00:12:58 -0500

    flask 2nd lesson

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -1,19 +1,30 @@
 # Import Flask modules
-
+from flask import Flask, render_template
 
 # Create an object named app 
 
+app = Flask(__name__)
 
 
 # Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
 # and assign to the route of ('/')
+@app.route('/')
+def head():
+    first = "This is my first conditions experince "
+    return render_template("index.html", message= first)
 
 
 
-# Create a function named header which prints numbers elements of list one by one in `index.html` 
-# and assign to the route of ('/')
+# Create a function named header which prints a welcome messages to users 
+# one by one in `body.html` and assign to the route of ('/mylist')
 
-
+@app.route('/mylist')
+def header():
+    names = ["salman", "Andre", "Jacop"]
+    return render_template("body.html", object = names)
 
 # run this app in debug mode on your local.
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
