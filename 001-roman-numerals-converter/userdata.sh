a663a4e9ac9a87ba1256a2cbaa48d60202524e6a
serkan210405 <serkanyagli2023@gmail.com>
2024-03-30 12:06:36 -0500

    web app

---
+++ 001-roman-numerals-converter/userdata.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+#update the OS
+
+yum update -y
+#install python and flask
+yum install python3 -y
+yum install pip -y
+pip3 install flask
+
+# install application files
+cd /home/ec2-user
+mkdir templates
+FOLDER="https://raw.githubusercontent.com/Serkan2104/my-repository/main/001-roman-numerals-converter"
+wget ${FOLDER}/app.py
+wget -P templates ${FOLDER}/templates/index.html
+wget -P templates ${FOLDER}/templates/result.html
+
+# install application files
+cd /home/ec2-user
+mkdir templates
+wget https://raw.githubusercontent.com/Serkan2104/my-repository/main/001-roman-numerals-converter/app.py
+wget -P templates https://raw.githubusercontent.com/Serkan2104/my-repository/main/001-roman-numerals-converter/templates/index.html
+wget -P templates https://raw.githubusercontent.com/Serkan2104/my-repository/main/001-roman-numerals-converter/templates/result.html
+
+#run application
+python3 app.py
