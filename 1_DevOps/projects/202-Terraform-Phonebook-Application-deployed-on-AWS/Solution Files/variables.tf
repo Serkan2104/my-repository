80a65db2d5b9cf1c33083a560b9b41091ebb77bf
serkan210405 <serkanyagli2023@gmail.com>
2024-07-14 11:51:58 -0500

    terraform project

---
+++ 1_DevOps/projects/202-Terraform-Phonebook-Application-deployed-on-AWS/Solution Files/variables.tf
@@ -0,0 +1,18 @@
+variable "hosted-zone" {
+    default = "XXXXXXXXXXX"  
+}
+
+variable "key-name" {
+    default = "XXXXXXXXXXX" 
+  
+}
+
+variable "git-token" {
+    default = "XXXXXXXXXXX" 
+  
+}
+
+variable "git-name" {
+    default = "XXXXXXXXXXX" 
+  
+}
